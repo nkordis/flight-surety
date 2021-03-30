c8a6414d6b4c3f0b059eba8837bfe14ad3b4860a
Nikos <nikoskordis@gmail.com>
2021-03-30 15:18:28 +0200

    refactor: Remove HDWalletProvider

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,10 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975
     }
   },
   compilers: {
